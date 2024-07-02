6d9cf060c951332369b72379b3bf03679250566e
MantashaTabassum13 <mantasha.tabassum16409@gmail.com>
2024-07-02 12:59:18 +0530

    NodeHealth

---
+++ nodeHealth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+############
+# Author : Mantasha
+# Date : 02/07/2024
+#
+# This script outputs the node health
+#
+# Version v1
+############
+#
+#
+
+set -x    #debug mode
+
+df -h
+
+free -g 
+
+nproc
+
+
